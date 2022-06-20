f465d859022b359842c3b0fd6b61c7bde55c5fb0
Ubaidullah <ubaid.99es@gmail.com>
2022-06-20 16:52:46 +0500

    static layout

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+
+export default function Navbar(){
+    return(
+        <nav>
+            <img src={require(`../images/Troll Face.png`)} alt="Troll Face" />
+            <h1>MemeGenerator</h1>
+        </nav>
+    )
+}
