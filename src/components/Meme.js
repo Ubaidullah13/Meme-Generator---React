6f900098bad42d4f233784415bdd1cce6569b43f
Ubaidullah <ubaid.99es@gmail.com>
2022-06-20 22:23:50 +0500

    Displaying Rand img

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,9 +1,30 @@
 import React from "react"
-
+import memesData from "../memesData.js"
+import defaultImg from "../images/memeimg.png"
 export default function Meme() {
+
+    const [memeImgs, setMemeImgs] = React.useState(memesData);
+    const [meme, setMeme] = React.useState({
+        topText: "SHUT UP",
+        bottomText: "AND TAKE MY MONEY",
+        randImg: defaultImg
+    })
+
+    function memeImage(){
+        const memesArray = memeImgs.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url;
+
+        setMeme( prev => ({
+                ...prev,
+                randImg: url
+
+        }));
+    }
+
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input 
                     type="text"
                     placeholder="Top text"
@@ -16,10 +37,13 @@ export default function Meme() {
                 />
                 <button 
                     className="form--button"
+                    onClick={memeImage}
                 >
                     Get a new meme image 🪄
                 </button>
-            </form>
+            </div>
+
+            <img src={meme.randImg} className="memeImg" alt=""/>
         </main>
     )
 }
